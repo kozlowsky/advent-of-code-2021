19a3bf4630feda133b95fbcf8a80e8778efef879
Jakub Kozlowski <jakub.kozlowski@pb.com>
2021-12-02 09:14:37 +0100

    ex 2.1

---
+++ src/main/scala/Day2.scala
@@ -0,0 +1,18 @@
+import scala.io.Source
+
+object Day2 {
+
+  def exercise1(input: List[String]): Int = {
+    val res = input.map(_.split(" ")).map(e => e(0) -> e(1).toInt).foldLeft(List(0, 0)){
+      case (acc, ("forward", i)) => List(acc(0), acc(1)+i)
+      case (acc, ("up", i)) => List(acc(0) - i, acc(1))
+      case (acc, ("down", i)) => List(acc(0) + i, acc(1))
+    }
+    res(0) * res(1)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val inputArray = Source.fromResource("day2.txt").getLines().toList
+    println(exercise1(inputArray))
+  }
+}
