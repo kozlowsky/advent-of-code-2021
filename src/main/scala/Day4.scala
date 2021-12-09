3e092db9de17b70004450da33c01d1d308b34a68
Jakub Kozlowski <jakub.kozlowski@pb.com>
2021-12-10 00:13:22 +0100

    ex 4.2 solution + refactor

--- src/main/scala/Day4.scala
+++ src/main/scala/Day4.scala
@@ -9,11 +9,11 @@ object Day4 {
     def exercise1Helper(bingoNumbers: List[Int], bingoTables: List[Array[Array[Int]]], currentState: Array[Array[Array[Int]]]): Int = {
       if (bingoNumbers.length <= 0) return -1
 
-      updateBingoBoardsState(bingoNumbers.head, bingoTables, currentState)
+      updateBingoTableState(bingoNumbers.head, bingoTables, currentState)
 
-      val winningBoardState = currentState.zipWithIndex.find(e => isTableWinning(e._1))
-      if (winningBoardState.isDefined) {
-        return calculateWinningValue(bingoNumbers.head, bingoTables(winningBoardState.get._2), winningBoardState.get._1)
+      val winningTableState = currentState.zipWithIndex.find(e => isTableWinning(e._1))
+      if (winningTableState.isDefined) {
+        return calculateWinningValue(bingoNumbers.head, bingoTables(winningTableState.get._2), winningTableState.get._1)
       }
 
       exercise1Helper(bingoNumbers.tail, bingoTables, currentState)
@@ -22,17 +22,50 @@ object Day4 {
     exercise1Helper(bingoNumbers, bingoTables, Array.ofDim[Int](bingoTables.length, bingoTables.head.length, bingoTables.head(0).length))
   }
 
+  def exercise2(bingoNumbers: List[Int], bingoTables: List[Array[Array[Int]]]): Int = {
+
+    @tailrec
+    def exercise2Helper(bingoNumbers: List[Int],
+                        bingoTables: List[Array[Array[Int]]],
+                        currentState: Array[Array[Array[Int]]] = Array.ofDim[Int](bingoTables.length, bingoTables.head.length, bingoTables.head(0).length),
+                        lastWinningTable: Array[Array[Int]] = Array(),
+                        lastWinningTableState: Array[Array[Int]] = Array(),
+                        lastWinningNumber: Int = -1): (Array[Array[Int]], Array[Array[Int]], Int) = {
+      if (bingoNumbers.isEmpty || bingoTables.isEmpty) return (lastWinningTable, lastWinningTableState, lastWinningNumber)
+
+      updateBingoTableState(bingoNumbers.head, bingoTables, currentState)
+
+      val winningTableState = currentState.zipWithIndex.filter(e => isTableWinning(e._1))
+      var (newWinningTable, newWinningTableState, newWinningBingoNumber, newCurrentState, newBingoTables) = (lastWinningTable, lastWinningTableState, lastWinningNumber, currentState, bingoTables)
+
+      winningTableState.foreach(e => {
+        newWinningTableState = e._1
+        newWinningTable = bingoTables(e._2)
+        newWinningBingoNumber = bingoNumbers.head
+
+        newBingoTables = newBingoTables.filterNot(_ sameElements newWinningTable)
+        newCurrentState = newCurrentState.filterNot(_ sameElements newWinningTableState)
+      })
+
+      exercise2Helper(bingoNumbers.tail, newBingoTables, newCurrentState, newWinningTable, newWinningTableState, newWinningBingoNumber)
+    }
+
+
+    val (lastWinningTable, lastWinningTableState, lastWinningNumber) = exercise2Helper(bingoNumbers, bingoTables)
+    calculateWinningValue(lastWinningNumber, lastWinningTable, lastWinningTableState)
+  }
+
   private def isTableWinning(bingoTable: Array[Array[Int]]): Boolean = {
     bingoTable.map(_.sum).contains(5) || bingoTable.transpose.map(_.sum).contains(5)
   }
 
-  private def calculateWinningValue(currentNumber: Int, currentBoard: Array[Array[Int]], currentBoardState: Array[Array[Int]]): Int = {
-    val sumOfNonPicked = currentBoard.zipWithIndex.map(e => e._1.zipWithIndex.filter(f => currentBoardState(e._2)(f._2) == 0).map(_._1).sum).sum
+  private def calculateWinningValue(currentNumber: Int, currentTable: Array[Array[Int]], currentTableState: Array[Array[Int]]): Int = {
+    val sumOfNonPicked = currentTable.zipWithIndex.map(e => e._1.zipWithIndex.filter(f => currentTableState(e._2)(f._2) == 0).map(_._1).sum).sum
 
     currentNumber * sumOfNonPicked
   }
 
-  def updateBingoBoardsState(currentBingoNumber: Int, bingoTables: List[Array[Array[Int]]], currentState: Array[Array[Array[Int]]]): Unit = {
+  def updateBingoTableState(currentBingoNumber: Int, bingoTables: List[Array[Array[Int]]], currentState: Array[Array[Array[Int]]]): Unit = {
     bingoTables.zipWithIndex.foreach(e => e._1.zipWithIndex
       .filter(f => f._1.contains(currentBingoNumber))
       .map(f => (f._1.indexOf(currentBingoNumber), f._2))
@@ -46,6 +79,7 @@ object Day4 {
     val (bingoNumbers, bingoTables) = readFile(args(0))
 
     println(exercise1(bingoNumbers, bingoTables))
+    println(exercise2(bingoNumbers, bingoTables))
   }
 
   private def readFile(path: String): (List[Int], List[Array[Array[Int]]]) = {
