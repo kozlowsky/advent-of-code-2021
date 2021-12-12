14ffb38ac037ac91be9871148f55d54a499580d6
Jakub Kozlowski <jakub.kozlowski@pb.com>
2021-12-12 19:39:41 +0100

    ex 5.1 optimization

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -5,27 +5,33 @@ object Day5 {
   def exercise1(points: List[((Int, Int), (Int, Int))]): Int = {
     val dimension = Math.round(points.flatMap(e => List(e._1._1, e._1._2, e._2._1, e._2._2)).max.ceil / 10) * 10
 
-    def generatePoints(points: ((Int, Int), (Int, Int)), nth: Int = 1000): List[(Int, Int)] = {
-      val smallerPoint = if (points._1._1 * nth + points._1._2 < points._2._1 * nth + points._2._2) points._1 else points._2
-      if(points._1._1 == points._2._1) {
-        (0 to Math.abs(points._1._2 - points._2._2)).map(e => (smallerPoint._1, smallerPoint._2 + e)).toList
+    def markPoints(points: ((Int, Int), (Int, Int)), diagram: Array[Array[Int]]): Unit = {
+      if (points._1._1 == points._2._1) {
+        (points._1._2 to points._2._2).foreach(e => diagram(e)(points._1._1) += 1)
       } else {
-        (0 to Math.abs(points._1._1 - points._2._1)).map(e => (smallerPoint._1 + e, smallerPoint._2)).toList
+        (points._1._1 to points._2._1).foreach(e => diagram(points._1._2)(e) += 1)
       }
     }
 
-    def fillDiagram(points: List[((Int, Int), (Int, Int))], diagram: Array[Array[Int]] = Array.ofDim[Int](dimension, dimension)): Int = {
-      points.map(generatePoints(_, dimension)).foreach(e => e.foreach(f => diagram(f._1)(f._2) += 1))
+    def fillDiagramAndGetSumOfOverlappingLines(points: List[((Int, Int), (Int, Int))], diagram: Array[Array[Int]] = Array.ofDim[Int](dimension, dimension)): Int = {
+      points.foreach(e => markPoints(e, diagram))
 
       diagram.map(e => e.count(_ > 1)).sum
     }
 
-    fillDiagram(points.filter(e => e._1._1 == e._2._1 || e._1._2 == e._2._2))
+    fillDiagramAndGetSumOfOverlappingLines(points.filter(e => e._1._1 == e._2._1 || e._1._2 == e._2._2))
   }
 
   def main(args: Array[String]): Unit = {
     val input = readFile(args(0))
-    println(exercise1(input))
+    println(exercise1(sortEachPair(input)))
+  }
+
+  private def sortEachPair(input: List[((Int, Int), (Int, Int))]) = {
+    input.map(e => {
+      if (e._1._1 + e._1._2 > e._2._1 + e._2._2) (e._2, e._1)
+      else (e._1, e._2)
+    })
   }
 
   private def readFile(fileName: String): List[((Int, Int), (Int, Int))] = {
